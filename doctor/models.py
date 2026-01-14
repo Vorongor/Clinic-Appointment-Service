772acd5c2a8c8077d6001f72cf1dd5677797167a
Mike <tradewcs@gmail.com>
2026-01-14 11:28:20 +0200

    fix flake8 errors

--- doctor/models.py
+++ doctor/models.py
@@ -4,10 +4,16 @@ from specializations.models import Specialization
 
 
 class Doctor(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    specializations = models.ManyToManyField(Specialization, related_name="doctors")
+    specializations = models.ManyToManyField(
+        Specialization,
+        related_name="doctors"
+    )
     price_per_visit = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
