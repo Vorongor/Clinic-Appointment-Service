414aaedec05698327d70133a58e5e29493b94cf3
Mike <tradewcs@gmail.com>
2026-01-14 13:39:55 +0200

    register model in admin

--- specializations/admin.py
+++ specializations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Specialization
+
+
+@admin.register(Specialization)
+class SpecializationAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "description")
+    search_fields = ("name", "code")
