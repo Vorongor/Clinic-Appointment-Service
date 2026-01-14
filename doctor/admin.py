98984b3c19e87390167392483f93b24feee664eb
Mike <tradewcs@gmail.com>
2026-01-14 13:28:28 +0200

    add tests

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Doctor
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "price_per_visit")
+    search_fields = ("first_name", "last_name")
+    filter_horizontal = ("specializations",)
