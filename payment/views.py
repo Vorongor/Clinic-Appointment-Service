c50cf5e35b6a5a59db17067cc35e3c97f37bdc21
Vladyslav Stasiuk <cheshire.cat.universe@gmail.com>
2026-01-15 12:19:26 +0200

    [Create] Payment view & serializer

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from payment.models import Payment
+from payment.serializers import PaymentSerializer
+
+
+@extend_schema(tags=["Payments"])
+class PaymentViewSet(ReadOnlyModelViewSet):
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        qs = Payment.objects.select_related("appointment", "appointment__patient")
+
+        if user.is_staff:
+            return qs
+        return qs.filter(appointment__patient=user)
