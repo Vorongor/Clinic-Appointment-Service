eddd350c080ffde7189584d6106e878909a58762
Nick <nikitosi3012@gmail.com>
2026-01-15 12:07:05 +0200

    Configure routes to env

--- notifications/telegram_helper.py
+++ notifications/telegram_helper.py
@@ -1,7 +1,9 @@
+import os
+
 import telebot
 from django.conf import settings
 
 
 def send_telegram_message(text: str):
-    bot = telebot.TeleBot(settings.TELEGRAM_BOT_TOKEN)
-    bot.send_message(settings.TELEGRAM_CHAT_ID, text)
+    bot = telebot.TeleBot(os.getenv("TELEGRAM_BOT_TOKEN"))
+    bot.send_message(os.getenv("TELEGRAM_CHAT_ID"))
